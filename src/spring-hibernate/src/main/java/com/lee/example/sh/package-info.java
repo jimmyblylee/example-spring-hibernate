60c5bba424244fe29e1ab24d7437301dd64ba3e0
Jimmybly Lee <jimmyblylee@126.com>
2016-09-11 00:54:24 +0800

    [snapshot] fix java-doc with package info

--- src/spring-hibernate/src/main/java/com/lee/example/sh/package-info.java
+++ src/spring-hibernate/src/main/java/com/lee/example/sh/package-info.java
@@ -1,14 +1,14 @@
 /**
- * Project Name : example-hibernate-jpa <br>
+ * Project Name : example-spring-hibernate <br>
  * File Name : package-info.java <br>
- * Package Name : com.lee.example.hj <br>
+ * Package Name : com.lee.example.sh <br>
  * Create Time : Sep 9, 2016 <br>
  * Create by : jimmyblylee@126.com <br>
  * Copyright © 2006, 2016, Jimmybly Lee. All rights reserved.
  */
 /**
  * ClassName : package-info <br>
- * Description : example ofr hibernate jpa<br>
+ * Description : example for spring + hibernate<br>
  * Create Time : Sep 9, 2016 <br>
  * Create by : jimmyblylee@126.com <br>
  *

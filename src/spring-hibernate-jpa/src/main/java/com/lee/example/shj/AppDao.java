1f2dd18aed7b261715fad072070dc91fcac6e241
Jimmybly Lee <jimmyblylee@126.com>
2016-09-11 00:45:50 +0800

    [snapshot] add example: spring + hibernate + JPA + c3p0 connecting to mysql there are three layers controller, service and dao, and i put the transaction at service layer.

---
+++ src/spring-hibernate-jpa/src/main/java/com/lee/example/shj/AppDao.java
@@ -0,0 +1,61 @@
+/**
+ * Project Name : example-spring-hibernate-jpa <br>
+ * File Name : AppDao.java <br>
+ * Package Name : com.lee.example.shj <br>
+ * Create Time : 2016-09-10 <br>
+ * Create by : jimmyblylee@126.com <br>
+ * Copyright © 2006, 2016, Jimmybly Lee. All rights reserved.
+ */
+package com.lee.example.shj;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.lee.example.h.entity.Group;
+import com.lee.example.h.entity.User;
+
+/**
+ * ClassName : AppDao <br>
+ * Description application dao <br>
+ * Create Time : 2016-09-10 <br>
+ * Create by : jimmyblylee@126.com
+ */
+@Repository
+public class AppDao {
+
+    @PersistenceContext(unitName = "data_mgmt")
+    private EntityManager em;
+    
+    @SuppressWarnings("unchecked")
+    public List<User> getAllUsers() {
+        return em.createQuery("from User").getResultList();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<Group> getAllGroups() {
+        return em.createQuery("from Group").getResultList();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<User> getAllUsersNoLazy() {
+        StringBuilder hql = new StringBuilder();
+        hql.append(" select distinct u from User as u");
+        hql.append(" left join fetch u.sex");
+        hql.append(" left join fetch u.role");
+        hql.append(" left join fetch u.groups");
+        hql.append(" left join fetch u.type");
+        return em.createQuery(hql.toString()).getResultList();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<Group> getAllGroupsNoLazy() {
+        StringBuilder hql = new StringBuilder();
+        hql.append("select distinct g from Group as g");
+        hql.append(" left join fetch g.users");
+        return em.createQuery(hql.toString()).getResultList();
+    }
+}
---
+++ src/spring-hibernate-jpa/src/main/java/com/lee/example/shj/AppService.java
@@ -0,0 +1,74 @@
+/**
+ * Project Name : example-spring-hibernate-jpa <br>
+ * File Name : AppService.java <br>
+ * Package Name : com.lee.example.shj <br>
+ * Create Time : Sep 10, 2016 <br>
+ * Create by : jimmyblylee@126.com <br>
+ * Copyright © 2006, 2016, Jimmybly Lee. All rights reserved.
+ */
+package com.lee.example.shj;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lee.example.h.entity.Group;
+import com.lee.example.h.entity.User;
+
+/**
+ * ClassName : AppService <br>
+ * Description : application service <br>
+ * Create Time : Sep 10, 2016 <br>
+ * Create by : jimmyblylee@126.com <br>
+ *
+ */
+@Service
+@Scope(value = SCOPE_PROTOTYPE)
+public class AppService {
+
+    @Resource
+    private AppDao dao;
+
+    /**
+     * Description : get all users with all properties <br>
+     * Create Time: 2016-09-10 <br>
+     * Create by : jimmyblylee@126.com <br>
+     *
+     * @return all users in the database
+     */
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers() {
+        return dao.getAllUsersNoLazy();
+    }
+
+    /**
+     * Description : get all groups with all properties <br>
+     * Create Time: 2016-09-10 <br>
+     * Create by : jimmyblylee@126.com <br>
+     *
+     * @return all groups in the database
+     */
+    @Transactional(readOnly = true)
+    public List<Group> getAllGroups() {
+        return dao.getAllGroupsNoLazy();
+    }
+
+    /**
+     * Description : get all the users for lazy properties test <br>
+     * Create Time: 2016-09-10 <br>
+     * Create by : jimmyblylee@126.com <br>
+     *
+     * @return all users with lazy properties which are not initialized
+     */
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers4LazyTest() {
+        return dao.getAllUsers();
+    }
+
+}
